e444c476685821cf746850dfc5f5237a54618240
seza443 <github.tanguy.vaessen@hotmail.com>
2015-02-09 19:54:47 +0100

    add files to repo

---
+++ mcp1/src/CT39V114.java
@@ -0,0 +1,52 @@
+//Ce fichier est encodé en UTF8.
+
+// VAESSEN Tanguy
+// LEPINOIS Loïc
+// LAMBIN Grégoire
+// HAUET Alexandre
+
+public class CT39V114 {
+  
+  // Le paramètre t n'est pas null. 
+  // De plus, il représente un vecteur non vide de n entiers relatifs.
+  
+  // Cette méthode ne modifie pas les éléments du tableau t.
+  // Elle renvoie la somme de la sous-sequence non vide maximum de t.
+	
+	public static long maxSum(int[] t) {
+		  int sum = Integer.MIN_VALUE;
+		  int currentSum = 0;
+		  for (int i =0; i < t.length; i++){
+			  currentSum += t[i];
+			  if (currentSum > sum){
+				  sum = currentSum;
+			  }else if(t[i]>sum){
+				  sum = t[i];
+				  currentSum = sum;
+			  }
+		  }
+
+		return sum;
+	  }
+  
+  //***********************************************************************************
+  //Expliquez la notion de "programme correct"
+	
+	// qui se termine
+	// qui respecte les préconditions
+
+  //***********************************************************************************
+  //Expliquez comment vous avez d√©velopp√© ce programme
+	
+	//analyser la situation
+	//exposer le problème sur papier
+	//identifier les deux versions demandées
+	
+  
+  //***********************************************************************************
+  //Expliquez pourquoi vous pensez que votre m√©thode est correcte
+	
+	//on a testé plusieurs cas sur papier
+	//elle respecte la complexité demandée
+	//
+}
---
+++ mcp1/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] a = {-4,-5,3,7,-5,100,-100,-100};
+		int[] b = {-100,-5,-3,-73};
+		
+		System.out.println(CT39V114.maxSum(a));
+		System.out.println(CT39V114.maxSum(b));
+
+	}
+
+}
